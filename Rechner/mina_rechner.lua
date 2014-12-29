d68f03bc99481919ed158d842f7815cb22d9bf16
Mina the Loli (=^-ω-^=) <djshira@live.de>
2014-12-29 08:04:15 +0100

    Update mina_rechner.lua

--- Rechner/mina_rechner.lua
+++ Rechner/mina_rechner.lua
@@ -1,8 +1,8 @@
-function rechner ()
+  function rechner ()
     print"Zahl eingeben:"
         a = io.read("*n")
     print"Welche Rechenart?"
-    print"[1]Plus, [2]Minus, [3]Mal, [4]Geteilt"
+    print"[1]Plus, [2]Minus, [3]Mal, [4]Geteilt, [5]Hoch"
         c = io.read("*n")
     print"Weitere Zahl eingeben:"
         b = io.read("*n")
@@ -21,13 +21,15 @@ function rechnung (c)
     elseif c == 3 then
         return a,"*",b,"=",a*b 
     elseif c == 4 then
-        return a,":",b,"=",a/b 
+        return a,":",b,"=",a/b
+     elseif c == 5 then
+        return a,"^",b,"=",a^b  
     elseif c < 1 then
         c = 1
         print"Falsche eingabe! Bitte wähle zwischen 1-4:"
             c = io.read("*n")
         print(rechnung(c))
-    elseif c > 4 then
+    elseif c > 5 then
         c = 1
         print"Falsche eingabe! Bitte wähle zwischen 1-4:"
             c = io.read("*n")
@@ -39,9 +41,14 @@ function beenden (d)
     if d == 1 then
         rechner()
     elseif d == 2 then
+        print"Minas LUA-Rechner wurde beendet!"
         os.exit()
     end
 end
 
-print"Rechner"
-rechner()   
+print"Minas LUA-Rechner"
+print"Möchten sie rechnen??"
+print"[1]Ja, [2]Nein"
+    d = io.read("*n")
+print(beenden(d))
+rechner()      
