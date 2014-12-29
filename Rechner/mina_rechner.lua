d863e5f552d3e85edeb8fe7eae749fad45d4e921
Mina the Loli (=^-ω-^=) <djshira@live.de>
2014-12-29 06:45:34 +0100

    Update mina_rechner.lua

--- Rechner/mina_rechner.lua
+++ Rechner/mina_rechner.lua
@@ -1,14 +1,15 @@
 function rechnung (c)
   if c == 1 then
-   return a + b
+   return a,"+",b,"=",a+b 
   elseif c == 2 then
-    return a - b
+    return a,"-",b,"=",a-b 
   elseif c == 3 then
-    return a * b
+    return a,"*",b,"=",a*b 
   elseif c == 4 then
-    return a / b
+    return a,":",b,"=",a/b 
   elseif c < 1 then
        c = 1
+       wait = 1
     print"Falsche eingabe! Bitte wähle zwischen 1-4:"
       c = io.read("*n")
 print(rechnung(c))
@@ -23,12 +24,12 @@ end
 print"Rechner"
 print"Zahl eingeben:"
   a = io.read("*n")
-print"Weitere Zahl eingeben:"
-  b = io.read("*n")
-print"Welche Rechenart?"
+  print"Welche Rechenart?"
 print"1 - Plus"
 print"2 - Minus"
 print"3 - Mal"
 print"4 - Geteilt"
   c = io.read("*n")
+print"Weitere Zahl eingeben:"
+  b = io.read("*n")
 print(rechnung(c))
